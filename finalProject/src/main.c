d3c021d36ccda4bb2436ee109ebecf3f58e2c8f9
= <=>
2020-12-11 10:49:15 -0800

    Starting MPI parallelization on seperate branch

--- finalProject/src/main.c
+++ finalProject/src/main.c
@@ -8,14 +8,42 @@
 int main(int argc, char* argv[]){
     char *kern, *img, *output;
     Matrix *kernel, *image, *final;
+    int my_rank, p, kheight, kwidth, iheight, iwidth;
 
     kern = argv[1];
     img = argv[2];
     output = argv[3];
 
-    kernel = get_matrix_from_file(kern);
-    image = get_matrix_from_file(img);
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+    if(my_rank == 0){
+      kernel = get_matrix_from_file(kern);
+      image = get_matrix_from_file(img);
+      kheight = kernel->height;
+      kwidth = kernel->width;
+      iheight = image->height;
+      iwidth = image->width;
+    }
+
+    MPI_Bcast(&kheight, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&kwidth, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&iheight, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&iwidth, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if(my_rank != 0){
+      kernel = create_matrix(kheight, kwidth):
+      image = create_matrix(iheight, iwidth);
+    }
+
+    MPI_Bcast(kernel.array, kheight*kwidth, MPI_INT, 0, MPI_COMM_WORLD);
+
     final = process_image(kernel, image);
+
+    MPI_Finalize();
 
     write_matrix_to_file(output, final);
 
