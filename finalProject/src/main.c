318e440a2815b11371678db69fabd1730c679738
Awstin <44249064+OrthogonalStar@users.noreply.github.com>
2020-12-13 15:18:55 -0800

    Update main.c
    Added timing

--- finalProject/src/main.c
+++ finalProject/src/main.c
@@ -1,5 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include <time.h>
 #include"matrix.h"
 #include"basic_functions.h"
 #include"file_io.h"
@@ -14,12 +15,18 @@ int main(int argc, char* argv[]){
     img = argv[3];
     output = argv[4];
 
+    clock_t start = clock();
     kernel_x = get_matrix_from_file(kern_x);
     kernel_y = get_matrix_from_file(kern_y);
     image = get_matrix_from_file(img);
     final = process_image(kernel_x, kernel_y, image);
 
+    float time_spent = (float)(clock() - start) / CLOCKS_PER_SEC;
+    printf("Serial code Done in %f seconds.\n", time_spent);
+    log_timing("serial", kernel_x->width, time_spent);
+    
     write_matrix_to_file(output, final);
 
     return 0;
 }
+
