c8df79218dc9e4e11bfebf5f6813a0eea3003f0d
ElenaVinokurova <helena.vinokurova@gmail.com>
2020-12-13 21:20:57 -0500

    Updated scrips

--- finalProject/scripts/filter.sh
+++ finalProject/scripts/filter.sh
@@ -1,11 +1,11 @@
-python3 ../../Python/image2MatrixTxt.py ../../finalProject/data/8k.jpeg 
+python3 ../../Python/image2MatrixTxt.py ../../finalProject/data/8k.jpg
 
 > timing_log.txt
 
-./main.x ../data/kernel_vertical_edge_detect_3x3.mat ../data/kernel_horizontal_edge_detect_3x3.mat ../data/8k.mat ../data/8k_filter3x3.mat
-./main.x ../data/kernel_vertical_edge_detect_5x5.mat ../data/kernel_horizontal_edge_detect_5x5.mat ../data/8k.mat ../data/8k_filter5x5.mat
-./main.x ../data/kernel_vertical_edge_detect_7x7.mat ../data/kernel_horizontal_edge_detect_7x7.mat ../data/8k.mat ../data/8k_filter7x7.mat
-./main.x ../data/kernel_vertical_edge_detect_9x9.mat ../data/kernel_horizontal_edge_detect_9x9.mat ../data/8k.mat ../data/8k_filter9x9.mat
+../build/main.x ../data/kernel_vertical_edge_detect_3x3.mat ../data/kernel_horizontal_edge_detect_3x3.mat ../data/8k.mat ../data/8k_filter3x3.mat
+../build/main.x ../data/kernel_vertical_edge_detect_5x5.mat ../data/kernel_horizontal_edge_detect_5x5.mat ../data/8k.mat ../data/8k_filter5x5.mat
+../build/main.x ../data/kernel_vertical_edge_detect_7x7.mat ../data/kernel_horizontal_edge_detect_7x7.mat ../data/8k.mat ../data/8k_filter7x7.mat
+../build/main.x ../data/kernel_vertical_edge_detect_9x9.mat ../data/kernel_horizontal_edge_detect_9x9.mat ../data/8k.mat ../data/8k_filter9x9.mat
 
 
 python3 ../../Python/viewImageFromText.py ../../finalProject/data/8k_filter3x3.mat 
